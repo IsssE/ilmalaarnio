2492c078b82b00eba39dcb52f9a92eb0b8d538e8
nikula2 <tommi.nikula@student.tut.fi>
2016-10-17 11:52:28 +0300

    connect alkeita

--- main.py
+++ main.py
@@ -2,21 +2,21 @@
 import paho.mqtt.client as mqtt
 
 class Connection:
-	def __init__(broker_ip, baddr = ""):
+	def __init__(self, broker_ip, baddr = ""):
 		self._ip = broker_ip
-		self.client = mqtt.Client(protocol=MQTTv31)
+		self.client = mqtt.Client(protocol="MQTTv31")
+
 		self.client.on_connect = self.on_connect
 		self.on_message = self.on_message
 
-		client.connect(broker_ip)
+		self.client.connect(broker_ip)
 
 	def on_connect(self, client, userdata, flags, rc):
-		#called when connected
-    	print "Connected with result code "+str(rc)
-    	client.subscribe("$SYS/#")
+		print ("Connected with result code "+str(rc))
+		self.client.subscribe("$SYS/#")
 
 
 	def on_message(self, client, userdata, msg):
-    	print msg.topic+" "+str(msg.payload)
+		print (msg.topic+" "+str(msg.payload))
 
 conn = Connection("54.93.150.126")
