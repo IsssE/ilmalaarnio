fd7da38fdfe55761b9a29b1285ca600b34fd2c11
nikula2 <tommi.nikula@student.tut.fi>
2016-10-18 15:48:56 +0300

    asd

--- constant.py
+++ constant.py
@@ -8,9 +8,16 @@ conn = connection.Connection("54.93.150.126")
 #cop = Copter(conn)
 
 #time.sleep(1)
+t = time.clock()
+
+direction = 0
 while True:
-	conn.send(0, 0, 2, 30)
-	time.sleep(0.2)
-	conn.send(0, 0, 1, 30)
-	time.sleep(0.2)
+	current_time = time.clock()
+
+	if (current_time - time > 10):
+		direction = 1
+	conn.send(1+ direction, 1+ direction, 2, 100)
+	time.sleep(100)
+	#conn.send(0, 0, 1, 30)
+	#time.sleep(0.2)
 	
