30622e619957bdd5283319b26d15bc14933b66c0
Ubuntu <ubuntu@ip-172-31-17-217.eu-central-1.compute.internal>
2016-10-18 07:17:36 +0000

    asd

--- main.py
+++ main.py
@@ -24,7 +24,9 @@ class Connection:
 	def on_subscribe(self, client, userfata, mid, granted_qos):
 		print("Subscribed")
 	def on_message(self, client, userdata, msg):
+		#print (msg)
 		self.messages.append(msg.payload)
+		print (self.messages)
 		if len(self.messages) > 100:
 			self.messages.pop(0)
 
@@ -46,11 +48,14 @@ class Copter:
 		self.m_up = 1 		#ascent motor: default speed
 		self.x_control = control
 
-	def horizontal(x):
+	def horizontal(self, x):
+		#print(x)
 		if (x > self.x_control):
+			print(str(x) > str(self.x_control))
 			self.conn.send(2,1, 2, 30)
 			time.sleep(0.5)
 		elif (x < (self.x_control * -1)):
+			print(str(x) < str(self.x_control * -1))
 			self.conn.send(1,2, 2, 30)
 			time.sleep(0.5)
 
@@ -60,11 +65,17 @@ class Copter:
 conn = Connection("54.93.150.126")
 cop = Copter(conn)
 
+time.sleep(1)
 #conn.send(0, 0, 1, 50)
 while True:
-	#conn.client.loop(timeout=1.0)
-	entry = json.load(conn.get())
-	Copter.horizontal(entry.x)
+	conn.client.loop()
+	entry = conn.get()
+	#print(entry)
+	if entry:
+		#print (entry)
+		json_entry = json.loads(entry)
+		#print(json_entry)
+		cop.horizontal(json_entry['x'])
 
 	#conn.send(0, 0, 2, 170)
 	#time.sleep(2)
