449b752fd3dae845a41d965914544157867c7a66
Ubuntu <ubuntu@ip-172-31-17-217.eu-central-1.compute.internal>
2016-10-18 12:14:32 +0000

    asd

--- navigation.py
+++ navigation.py
@@ -12,7 +12,7 @@ import localization as lx
 
 class Navigation:
 	def __init__(self):
-		self.beacons = []
+		self.beacons = {}
 		self.p = lx.Project(mode='3D')
 
 
@@ -24,29 +24,29 @@ class Navigation:
 	def strength_to_distance(self, strength):
 		#return approximated distance to beacon
 		#todo: kaava
-		return strength()
+		return float(strength)
 
 	def get_position(self, beacon_s):
 		#returns np.array(x,y,z): approximate position. 
 		#beacon_s: list of [(baddr, strength), (baddr, strength)]
 		b1, b2, b3 = beacon_s
-		dist_b1 = strength_to_distance(b1[1])
-		dist_b2 = strength_to_distance(b2[1])
-		dist_b3 = strength_to_distance(b3[1])
+		dist_b1 = self.strength_to_distance(b1[1])
+		dist_b2 = self.strength_to_distance(b2[1])
+		dist_b3 = self.strength_to_distance(b3[1])
 
 		#coord_b1 = self.beacons[b1[0]]
-		t, label = p.add_target()
-		t.add_measure(b1[0], dist_b1)
-		t.add_measure(b2[0], dist_b2)
-		t.add_measure(b3[0], dist_b3)
+		t, label = self.p.add_target()
+		t.add_measure("1", dist_b1)
+		t.add_measure("2", dist_b2)
+		t.add_measure("3", dist_b3)
 
-		P.solve()
+		self.p.solve()
 		return(t.loc.x, t.loc.y, t.loc.z)
 
 n = Navigation()
 n.add_beacon("1", (0, 0, 0))
-n.add_beacon("2", (1, 1, 1))
-n.add_beacon("3", (1, 0, 0))
+n.add_beacon("2", (2, 2, 2))
+n.add_beacon("3", (2, 0, 0))
 
-print(n.get_position([(1, 1), (2, 1), (3, 1)]))
+print(n.get_position([(1, 1.4), (2, 1.4), (3, 1.4)]))
 
